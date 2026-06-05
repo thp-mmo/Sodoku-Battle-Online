f61f2550e78d80bdab6f39704b8fafe4cd2fe8e5
ThangZake <trantienthang18082006@gmail.com>
2026-06-05 21:37:37 +0700

    Tao class Board, Cell, Player, Room, Tao giao dien UI, Timer

---
+++ src/SudokuBattleOnline/Shared/Models/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public int Value { get; set; }
+
+        public bool IsFixed { get; set; }
+    }
+}
---
+++ src/SudokuBattleOnline/Shared/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Player
+    {
+        public string Username { get; set; }
+
+        public int Score { get; set; }
+
+        public int Mistakes { get; set; }
+
+        public bool IsReady { get; set; }
+
+        public bool IsWinner { get; set; }
+    }
+}
